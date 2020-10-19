dbe54403973004a7586248b74db78ffa2154afec
Niki Brown <nikibrown@Nikis-MacBook-Pro.local>
2020-10-18 17:06:00 -0700

    add bottom banner cta

--- page.php
+++ page.php
@@ -30,54 +30,65 @@ get_header();
 	
 
 	<main>
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<nav aria-label="breadcrumb">
-							<?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
-						</nav>
-					</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<nav aria-label="breadcrumb">
+						<?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
+					</nav>
 				</div>
-
-				<div class="row no-gutters">
-					<div class="col-lg-3 d-none d-lg-block">
-						<aside class="sidebar-nav">
-							<nav>
-							<?php
-								global $post;
-								$parents = get_post_ancestors( $post->ID );
-								/* Get the ID of the 'top most' Page if not return current page ID */
-								$id = ($parents) ? $parents[count($parents)-1]: $post->ID;
-
-								$childPages = wp_list_pages( 'title_li=&child_of='.$id.'&echo=0' );
-								if ( $childPages) : ?>
-									<ul class="list-unstyled">
-										<?php echo $childPages; ?>
-									</ul>
-								<?php endif; ?>
-							</nav>
-							
-						</aside>
-					</div>
-					<div class="col-lg-9">
-						<article>
-							<?php if( get_field('page_intro') ): ?>
-								<p class="intro"><?php the_field("page_intro"); ?></p>
-							<?php endif; ?>
-
-							<?php the_content(); ?>
-
-								
-						</article>
-						
-	
-					</div>
-				</div>
-						
-						
-				
 			</div>
-		</main>
+
+			<div class="row no-gutters">
+				<div class="col-lg-3 d-none d-lg-block">
+					<aside class="sidebar-nav">
+						<nav>
+						<?php
+							global $post;
+							$parents = get_post_ancestors( $post->ID );
+							/* Get the ID of the 'top most' Page if not return current page ID */
+							$id = ($parents) ? $parents[count($parents)-1]: $post->ID;
+
+							$childPages = wp_list_pages( 'title_li=&child_of='.$id.'&echo=0' );
+							if ( $childPages) : ?>
+								<ul class="list-unstyled">
+									<?php echo $childPages; ?>
+								</ul>
+							<?php endif; ?>
+						</nav>
+						
+					</aside>
+				</div>
+				<div class="col-lg-9">
+					<article>
+						<?php if( get_field('page_intro') ): ?>
+							<p class="intro"><?php the_field("page_intro"); ?></p>
+						<?php endif; ?>
+
+						<?php the_content(); ?>
+					</article>
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<?php if( get_field('bottom_banner_cta_headline') ): ?>
+		<section class="bottom-banner-cta">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-lg-9">
+						<h2><?php the_field("bottom_banner_cta_headline"); ?></h2>
+						<?php the_field("bottom_banner_cta_text"); ?>
+					</div>
+					<div class="col-lg-3">
+						<a href="<?php the_field("bottom_banner_cta_button_link"); ?>" class="btn btn-primary"><?php the_field("bottom_banner_cta_button_text"); ?> <i class="fas fa-angle-right"></i>
+							</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	<?php endif; ?>
+
 
 <?php endwhile; endif; ?>
 
