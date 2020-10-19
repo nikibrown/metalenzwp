d61d129f8fb52e1322c81cb825dc465286bef0f8
Niki Brown <nikibrown@Nikis-MacBook-Pro.local>
2020-10-19 06:44:28 -0700

    add logic to hide contents of page subheader if empty to just show breadcrumbs

--- template-parts/content-page-subheader.php
+++ template-parts/content-page-subheader.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<?php if( get_field('page_intro') ): ?>
+
 	<!-- page-subheader-blue -->
 
 	<section class="page-subheader 
@@ -18,28 +18,35 @@
 		<?php } else { ?>
 			page-subheader-blue
 		<?php } ?>
-	">
+		">
 		<div class="container">
-			<div class="row">
+			<div class="row
+				<?php if( get_field('page_intro') ): ?>
+					row-min-height
+				<?php endif; ?>
+			"> 
 				<div class="col-lg-6">
 					<nav aria-label="breadcrumb">
 						<?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
 					</nav>
 
-					<div class="text-container">
-						<h2><?php the_field("page_intro"); ?></h2>
-					</div>
+					<?php if( get_field('page_intro') ): ?>
+						<div class="text-container">
+							<h2><?php the_field("page_intro"); ?></h2>
+						</div>
+					<?php endif; ?>
 				</div>
 				<div class="col-lg-1"></div>
 				<div class="col-lg-5">
-					<?php if( get_field('page_subheader_image') ) { ?>
-						<div class="page-subheader-img" style="background-image: url('<?php the_field("page_subheader_image"); ?>');">
-					<?php } else { ?>
-						<div class="page-subheader-img" style="background-image: url('<?php bloginfo("template_directory")?>/assets/img/subheader.jpg');">
-					<?php } ?>
+					<?php if( get_field('page_intro') ): ?>
+						<?php if( get_field('page_subheader_image') ) { ?>
+							<div class="page-subheader-img" style="background-image: url('<?php the_field("page_subheader_image"); ?>');">
+						<?php } else { ?>
+							<div class="page-subheader-img" style="background-image: url('<?php bloginfo("template_directory")?>/assets/img/subheader.jpg');">
+						<?php } ?>
+					<?php endif; ?>
 					</div>
 				</div>
 			</div>
 		</div>
 	</section>
-<?php endif; ?>
