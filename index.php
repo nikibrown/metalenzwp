2970030934fbc6d1a583f5fae714c6d3efc2e56e
Niki Brown <nikibrown@Nikis-MacBook-Pro.local>
2020-10-19 17:14:09 -0700

    add posts page and single page

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<?php
+<?php 
 /**
- * The main template file
+ * The template for displaying news & Events
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -15,43 +15,72 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
 
-		<?php
-		if ( have_posts() ) :
+	<?php if( get_field('page_header_image') ) { ?>
+		<section class="page-header-img" style="background-image: url('<?php the_field("page_header_image"); ?>);">
+	<?php } else { ?>
+		<section class="page-header-img" style="background-image: url('<?php bloginfo("template_directory")?>/assets/img/masthead.jpg);">
+	<?php } ?>
+		<div class="container">
+			<div class="row align-items-end">
+				<div class="col-lg-12">
+					<?php $title = get_post_field( 'post_title', 70 ); ?>
+					<h1><?php echo $title; ?></h1>
+				</div>	
+			</div>
+			
+		</div>
+	</section>
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+	<main class="page-sidebar">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<nav aria-label="breadcrumb">
+						<?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
+					</nav>
+				</div>
+			</div>
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+			<div class="row">
+				<div class="col-12">
+					<article>
+						<?php if( get_field('page_intro') ): ?>
+							<p class="intro"><?php the_field("page_intro", 70); ?></p>
+						<?php endif; ?>
+						<ul class="list-unstyled news-events">
+						
+						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+							
+								<li class="row">
+									<div class="col-lg-4">
+										<?php if ( has_post_thumbnail() ) { ?>
+											<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+												<?php the_post_thumbnail(); ?>
+											</a>
 
-			endwhile;
+										<?php } else { ?>
+											<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+												<img src="<?php bloginfo("template_directory")?>/assets/img/news.jpg" alt="<?php the_title_attribute(); ?>">
+											</a>
+										<?php } ?>
+									</div>
+									<div class="col-lg-8">
+										<div class="news-inner">
+											<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+											<span class="date"><?php echo get_the_date( 'F j, Y' ); ?></span>
+											<?php the_excerpt(); ?>
+										</div>
+									</div>
+								</li>
+						<?php endwhile; endif; ?>
+						</ul>
+					</article>
+					
+				</div>
+			</div>
+		</div>
+	</main>
 
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -10,31 +10,46 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<?php 
+/**
+ * The template for displaying news & Events
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package m
+ */
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+get_header();
+?>
 
-			get_template_part( 'template-parts/content', get_post_type() );
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>	
+	<?php get_template_part( 'template-parts/content', 'page-header' ); ?>	
+	<main class="page-sidebar">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<nav aria-label="breadcrumb">
+						<?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
+					</nav>
+				</div>
+			</div>
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'mwp' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'mwp' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+			<div class="row">
+				<div class="col-12">
+					<article>
+						<?php the_content(); ?>
+					</article>
+					
+				</div>
+			</div>
+		</div>
+	</main>
+<?php endwhile; endif; ?>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+<?php get_footer(); ?>
 
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
