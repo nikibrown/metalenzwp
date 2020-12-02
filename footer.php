50fc6e7d3d980171e01aafdd7f9fd492cf80481d
Niki Brown <nikibrown@Niki-Browns-MacBook-Pro.local>
2020-12-02 08:54:14 -0800

    Adjust sub-nav header/footer styling for mobile

--- footer.php
+++ footer.php
@@ -15,6 +15,21 @@
 			<div class="container">
 				<div class="row align-items-start">
 					<div class="col-lg-3">
+
+						<div class="sub-nav">
+							<ul class="list-inline">
+								<li class="list-inline-item">
+									<form role="search" action="<?php echo home_url( '/' ); ?>" method="get" class="header-search-form form-inline my-2 my-lg-0">
+										<input class="header-top-search form-control mr-sm-2" type="Search" value="<?php the_search_query(); ?>" aria-label="Search" name="s" id="s">
+										<span id="search-label" class="search-label d-inline-block">Search <i class="fas fa-search"></i></span>
+									</form>
+								</li>
+								<li class="list-inline-item"><a href="<?php the_field('header_contact_link', 'option'); ?>">Contact <i class="fas fa-at"></i></a></li>
+							</ul>
+
+						</div>
+
+
 						<?php the_field('footer_column_1', 'option'); ?>
 					</div>
 					<div class="col-lg-3">
